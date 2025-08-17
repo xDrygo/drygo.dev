0392c47c103483759acfc693b70f22498224e2a7
xDrygo <xdrygo-personal@outlook.com>
2025-08-16 23:22:21 -0700

    💫 Callback attempt 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react()],
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
