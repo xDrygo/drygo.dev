5217459e99a97e9cb2ff0226ce96160c9522ca9f
xDrygo <xdrygo-personal@outlook.com>
2026-03-29 23:09:01 -0700

    portfolio v3.0

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
